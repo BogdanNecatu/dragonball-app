5f5ac74d8749ab5a91a1f63db05987c512784a21
BogdanNecatu <necatubogdan@gmail.com>
2025-04-27 02:10:56 +0200

    Chore: Unify global styles

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,6 +1,7 @@
 import { Outlet } from 'react-router-dom';
 import Header from '../widgets/Header/Header';
 import styles from './App.module.css';
+import '../shared/styles/globalStyles.css';
 
 const App = () => {
   return (
